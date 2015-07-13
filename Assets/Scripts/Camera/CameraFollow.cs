7b7f00bc97ddfa3d464e94881b28fa0218f37024
Paul <hakuliu@gmail.com>
2015-07-13 15:06:33 -0700

    camera movement
    now follows where the player is facing. 3rd person style

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -18,7 +18,16 @@ public class CameraFollow : MonoBehaviour {
 
 	void FixedUpdate()
 	{
-		Vector3 targetpos = target.position + offset;
+		Vector3 targetpos = target.TransformDirection (Vector3.forward);
+		targetpos = -1 * targetpos.normalized;//pretty sure it already is...w/e
+		targetpos *= offsetBack;
+		targetpos.y += offsetHeight;
+		targetpos += target.position;
+
+		Vector3 lookto = target.position - targetpos;
+		Quaternion targetrot = Quaternion.LookRotation (lookto);
+
 		transform.position = Vector3.Lerp (transform.position, targetpos, smoothing * Time.deltaTime);
+		transform.rotation = Quaternion.Lerp (transform.rotation, targetrot, smoothing * Time.deltaTime);
 	}
 }
