28e53c066a64041c821547e4c19e5fde7151c905
Paul <hakuliu@gmail.com>
2015-07-25 02:58:32 -0700

    skeleton for shield

---
+++ Assets/Scripts/Player/HoldRelease/ShieldAction.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class ShieldAction : AbstractHoldReleaseAction
+{
+	public ShieldAction() : base(KeyCode.Mouse1 {
+
+	}
+	protected override void doInitialAction ()
+	{
+
+	}
+	protected override void doReleasedAction ()
+	{
+
+	}
+}
+
