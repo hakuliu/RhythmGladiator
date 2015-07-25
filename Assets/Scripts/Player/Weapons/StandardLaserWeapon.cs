49bae0f9f44e4cc18c5a82605140734a1e9ee943
Paul <hakuliu@gmail.com>
2015-07-25 11:22:58 -0700

    moved lasering into its own class.

---
+++ Assets/Scripts/Player/Weapons/StandardLaserWeapon.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using System.Collections;
+
+public class StandardLaserWeapon : AbstractWeapon
+{
+	public float range = 100f;
+	
+	
+	Ray shootRay;
+	RaycastHit shootHit;
+	int shootableMask;
+	ParticleSystem gunParticles;
+	LineRenderer gunLine;
+	AudioSource gunAudio;
+	Light gunLight;
+	float effectsDisplayTime = 0.2f;
+
+	protected override void Start ()
+	{
+		base.Start ();
+		shootableMask = LayerMask.GetMask ("Shootable");
+		gunParticles = GetComponent<ParticleSystem> ();
+		gunLine = GetComponent <LineRenderer> ();
+		gunAudio = GetComponent<AudioSource> ();
+		gunLight = GetComponent<Light> ();
+
+	}
+
+	void Update() {
+		if(playervars.GlobalAttackTimer >= playervars.timeBetweenGlobalAttacks * effectsDisplayTime)
+		{
+			DisableEffects ();
+		}
+	}
+	public override void doNormalAttack ()
+	{
+		Shoot ();
+	}
+
+	public override void doWomboAttack ()
+	{
+
+	}
+
+	public void DisableEffects ()
+	{
+		gunLine.enabled = false;
+		gunLight.enabled = false;
+	}
+	
+	
+	void Shoot ()
+	{
+		playervars.resetGlobalAttack ();
+		
+		gunAudio.Play ();
+		
+		gunLight.enabled = true;
+		
+		gunParticles.Stop ();
+		gunParticles.Play ();
+		
+		gunLine.enabled = true;
+		gunLine.SetPosition (0, transform.position);
+		
+		shootRay.origin = transform.position;
+		shootRay.direction = transform.forward;
+		
+		if(Physics.Raycast (shootRay, out shootHit, range, shootableMask))
+		{
+			EnemyHealth enemyHealth = shootHit.collider.GetComponent <EnemyHealth> ();
+			if(enemyHealth != null)
+			{
+				enemyHealth.TakeDamage (dmg, shootHit.point);
+			}
+			gunLine.SetPosition (1, shootHit.point);
+		}
+		else
+		{
+			gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
+		}
+	}
+}
+
