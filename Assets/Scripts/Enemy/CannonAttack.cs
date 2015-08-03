605a02385b54b3b9a86e75c5519036969cb24d38
Paul <hakuliu@gmail.com>
2015-08-02 23:22:59 -0700

    a variable

--- Assets/Scripts/Enemy/CannonAttack.cs
+++ Assets/Scripts/Enemy/CannonAttack.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class CannonAttack : AbstractSingleAttack
 {
+	public float beatsToHit = 2f;
 	public float heightoffset = .2f;
 	public AudioClip[] shootingSounds;
 	SoundRandomizer soundpicker;
@@ -21,7 +22,7 @@ public class CannonAttack : AbstractSingleAttack
 		
 		GameObject projectileinstance = Instantiate(projectile, this.transform.position + upabit, this.transform.rotation) as GameObject;
 		AbstractProjectileScript b = projectileinstance.GetComponent<AbstractProjectileScript> ();
-		b.setTrackDelaysAndStart (new float[]{4f});
+		b.setTrackDelaysAndStart (new float[]{beatsToHit});
 		soundpicker.PlayNext ();
 	}
 }
