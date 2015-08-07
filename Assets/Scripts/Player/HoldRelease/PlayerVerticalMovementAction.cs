c402dfda56282a90da3d4eb0f897f1a65e42417f
Paul <hakuliu@gmail.com>
2015-08-07 12:55:04 -0700

    jumping, hovering, and stomping preliminaries.

--- Assets/Scripts/Player/HoldRelease/PlayerVerticalMovementAction.cs
+++ Assets/Scripts/Player/HoldRelease/PlayerVerticalMovementAction.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class PlayerVerticalMovementAction : AbstractHoldReleaseAction
 {
+	public float jumpForce = 1.5f;
+	public float jumpDecay = 1.5f;
 	private Rigidbody playerRigidbody;
 	private PlayerVars playervars;
-	private float jumpForce = 30f;
 	private float timeBetweenJump = 1f;
 	private float timerJump = 0;
 	private bool jumping;
+	private Vector3 vel;
 
 	public PlayerVerticalMovementAction(Rigidbody player, PlayerVars vars) : base(KeyCode.E) {
 		this.playerRigidbody = player;
@@ -21,25 +23,45 @@ public class PlayerVerticalMovementAction : AbstractHoldReleaseAction
 	}
 	protected override void doReleasedAction ()
 	{
-		//experimental
-		Debug.Log ("rawr");
+		Stomp ();
 	}
 
 	public override void FixedUpdate ()
 	{
 		base.FixedUpdate ();
 		timerJump += Time.fixedDeltaTime;
-	}
+		JumpUpdate ();
+		if (pressed) {
+			playerRigidbody.useGravity = false;
+		} else {
+			playerRigidbody.useGravity = true;
+		}
 
+	}
+	void JumpUpdate() {
+		if (jumping) {
+			playerRigidbody.MovePosition(playerRigidbody.transform.position + vel);
+			vel /= jumpDecay;
+			if(vel.magnitude <= .2) {
+				vel = Vector3.zero;
+				jumping = false;
+			}
+		}
+	}
 	void Jump ()
 	{
 		if (timerJump >= timeBetweenJump) {
 			timerJump = 0;
 			Vector3 up = playerRigidbody.transform.TransformDirection(Vector3.up);
-			//playerRigidbody.MovePosition(transform.position + up * 5);
-			playerRigidbody.AddForce(up * jumpForce, ForceMode.Impulse);
+			vel = up * jumpForce;
+			jumping = true;
 		}
 		
+	}
+	void Stomp() {
+		Vector3 tempos = playerRigidbody.transform.position;
+		tempos.y = 0f;
+		playerRigidbody.MovePosition (tempos);
 	}
 }
 
